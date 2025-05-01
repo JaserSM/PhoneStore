91fde8712effd85ecbb15ef7e4c11c47a49d4892
JM-Santo <jmsanto@inired.com>
2025-05-01 13:07:29 +0200

    Added navigation back to HomePAge

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,13 +1,19 @@
+import { useNavigate } from "react-router";
 import Search from "../search/Search";
 import React from "react";
 
 
 const Layout = ({ children, search }) => {
+  const navigate = useNavigate();
 
+  const handleHeaderClick = () => {
+    console.log("click");
+    navigate(`/home`);
+  };
 
   return (
     <div className="App">
-      <header>
+      <header onClick={handleHeaderClick}>
         <h1>{">"} Phone Store </h1>
         <h2>{">"} &nbsp; Tu tienda de teléfonos online </h2>
       </header>
